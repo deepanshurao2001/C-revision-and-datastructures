0201185cceb0abee103960c7c11fc102991974dd
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-07-21 23:33:17 +0530

    swap_element_of_array_final

--- swap_alternate.cpp
+++ swap_alternate.cpp
@@ -1,15 +1,10 @@
 #include<iostream>
 using namespace std;
 void swapalternative(int arry[], int size){
-    if(size%2!=0){
-    for(int i=0;i<size-1;i+=2){
+    for(int i=0;i<size;i+=2){
+        if(i+1 < size){
        swap(arry[i],arry[i+1]);
-    }
-    }
-    else{
-        for(int i=0;i<size-1;i+=2){
-       swap(arry[i],arry[i+1]);
-    }
+        }
     }
 }
 void printarray(int arry[], int size){
@@ -18,7 +13,7 @@ void printarray(int arry[], int size){
     }
 } 
 int main(){
-    int arry[]={1,2,3,4,5,6,7,8,9,10};
+    int arry[]={1,2,3,4,5,6,7,8,9};
 swapalternative(arry, sizeof(arry)/sizeof(int));
 cout<<endl;
 printarray(arry,sizeof(arry)/sizeof(int));
