edad5f9b2e71e246f59ad08a20b4f52e199f496a
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-07-21 23:28:03 +0530

    if we have odd number of element in array

--- swap_alternate.cpp
+++ swap_alternate.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 void swapalternative(int arry[], int size){
-    for(int i=0;i<size;i+=2){
+    if(size%2!=0){
+    for(int i=0;i<size-1;i+=2){
        swap(arry[i],arry[i+1]);
+    }
+    }
+    else{
+        for(int i=0;i<size-1;i+=2){
+       swap(arry[i],arry[i+1]);
+    }
     }
 }
 void printarray(int arry[], int size){
@@ -11,7 +18,7 @@ void printarray(int arry[], int size){
     }
 } 
 int main(){
-    int arry[]={1,2,3,4,5,6,7,8};
+    int arry[]={1,2,3,4,5,6,7,8,9,10};
 swapalternative(arry, sizeof(arry)/sizeof(int));
 cout<<endl;
 printarray(arry,sizeof(arry)/sizeof(int));
