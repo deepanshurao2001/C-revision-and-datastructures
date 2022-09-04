34e28da3e7daf7728a37a829707ea333b75b86ba
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-09-04 21:46:15 +0530

    string is valid palindrome

---
+++ Valid_palindrome.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+bool valid(char ch){
+    if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')||(ch>='0' && ch<='9')){
+        return 1;
+    }
+    return 0;
+}
+char toLowerCase(char ch){
+    if((ch>='a' && ch<='z')||(ch>='a' && ch<='z'))
+    return ch;
+    else{
+        char temp = ch -'A'+ 'a';
+        return temp;
+    }
+}
+bool checkPalandrome(string a){
+    int s=0;
+    int e=a.length()-1;
+    while(s<=e){
+        if( a[s]  !=  a[e])
+        {
+            return 0;
+        }
+        else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+bool isPalindrome(string s){
+    string temp = " ";
+    for(int j=0; j<s.length();j++){
+        if(valid(s[j])){
+            temp.push_back(s[j]);
+        }
+    }
+    for(int j=0; j<temp.length(); j++){
+        temp[j] = toLowerCase(temp[j]);
+    }
+    return checkPalandrome(temp);
+}
+int main()
+{
+    string s =" ";
+    cout<<isPalindrome(s);
+}
