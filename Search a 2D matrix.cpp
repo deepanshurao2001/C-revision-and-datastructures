b95e18de376ab3ee5dec6cacfa400fd3c97c15cd
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-09-21 21:58:50 +0530

    search in 2D array

---
+++ Search a 2D matrix.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool searchMatrix(vector<vector<int>>& matrix, int target){
+    int row = matrix.size();
+    int col = matrix[0].size();
+    int start = 0;
+    int end = row*col-1;
+    int mid = start +(end-start)/2;
+    while(start<=end){
+        int element = matrix[mid/col][mid%col];
+        if(element == target){
+            return 1;
+        }
+        if(element < target){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = start +(end-start)/2;
+    }
+    return 0;
+}
+int main(){
+    vector<vector<int>> arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    cout<<searchMatrix(arr,7);
+}
