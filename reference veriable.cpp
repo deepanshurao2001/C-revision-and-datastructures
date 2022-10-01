43dec51722534434c011aa29f93b02091ec4b650
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-10-01 15:46:14 +0530

    reference variable

---
+++ reference veriable.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void update(int &n){
+    n++;
+}
+int main(){
+    int i=5;
+    int &j=i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    int n=5;
+    cout<<"Brfore"<<n<<endl;
+    update(n);
+    cout<<"After "<<n<<endl;
+}
