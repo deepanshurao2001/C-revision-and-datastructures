0cd178ee05ba4c1eaf7030ca623de4657b83f99a
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-10-01 21:49:27 +0530

    dynamic memory

---
+++ dynamic memory .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int getSum(int *arr,int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    //variable size of array
+    int *arr = new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Answer is:"<<getSum(arr,n)<<endl;
+
+}
