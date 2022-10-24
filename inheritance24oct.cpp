25d91b99f1fd1562771ee67fb95cf1f5e1a5a0f1
Deepanshurao2001 <raodeepanshu99@gmail.com>
2022-10-24 22:30:41 +0530

    encapculation and inheritance

---
+++ inheritance24oct.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Human{
+    public:
+    int height;
+    int weight;
+    int age;
+    public:
+    int getAge(){
+        return this->age;
+    }
+    int setweight(int w){
+        this->weight= w;
+
+    }
+};
+class Male: public Human{
+    public:
+    string color;
+    void sleep(){
+        cout<<"Male Sleeping"<<endl;
+    }
+};
+int main(){
+    Male object1;
+    cout<<object1.age << endl;
+    cout<<object1.weight<<endl;
+    cout<<object1.color<<endl;
+    object1.setweight(60);
+    cout<<object1.weight<<endl;
+    object1.sleep();
+}
